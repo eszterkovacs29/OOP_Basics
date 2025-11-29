5b49ce21252a4b5308872827a10e87b9508d1170
eszterkovacs29 <eszter.kovacs9801@gmail.com>
2025-11-29 09:47:04 +0100

    Added Function

--- OOP_Basics/Program.cs
+++ OOP_Basics/Program.cs
@@ -8,9 +8,21 @@ namespace OOP_Basics
 {
     internal class Program
     {
+        static void WelcomeUser(string name)
+        {
+            Console.WriteLine($"Hello {name}!");
+        }
+
+        static int Square(int number)
+        {
+            return number * number;
+        }
+
         static void Main(string[] args)
         {
-
+            Console.WriteLine(Square(5));
+            Console.WriteLine(Square(9));
+            Console.WriteLine(Square(13));
             Console.ReadKey();
         }
     }
